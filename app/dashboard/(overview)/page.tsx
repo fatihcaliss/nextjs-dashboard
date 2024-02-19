84fcc2c56de71877013c1e9ed621446c90de1796
fatihcaliss <fthcls@gmail.com>
2024-02-19 15:49:16 +0300

    chapter 16 - Adding Metadata

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -9,6 +9,11 @@ import {
   RevenueChartSkeleton,
   CardsSkeleton,
 } from '@/app/ui/skeletons';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
 
 export default async function Page() {
   const {
